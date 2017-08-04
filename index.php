47a6dc4eab4e463e274060634da4170237a1fa9a
Vuong <ndwuong2@gmail.com>
2017-08-04 17:58:19 +0700

    Created demo index page

--- index.php
+++ index.php
@@ -1,42 +1,176 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Hello</title>
+	<meta charset="UTF-8">
+	<title>Elight Page</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="public/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="public/font/trade-gothic-no-2-bold-1361519963.ttf">
+	<link rel="stylesheet" href="public/jquery/jquery.js">
+	<link rel="stylesheet" href="public/css/style.css">
 </head>
-<meta name="charset" content="utf-8">
-<style type="text/css" media="screen">
-	.container {
-		position: relative;
-	}
-	.img:hover {
-		-webkit-filter: brightness(50%);
-		filter: brightness(40%);
-	}
-	.text{
-		position: absolute;
-		top: 45%;
-		left: 10%;
-		color: white;
-		text-align: center;
-	}
-</style>
 <body>
-<?php echo 'Hello World'; ?>
-<div class="container">
-	<img class="img" src="img/guitar-01.jpg" alt="" onmouseenter="show()" onmouseleave="hide()">
-	<p class="text" id="01">This is the text when hover on the image</p>
-</div>
+	<div class="header navbar-fixed-top">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 header1">
+					<img src="public/images/landing-page-test2.png" alt="">
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 header2">
+					<div class="btn">
+						<button><p>Cài đặt miễn phí</p><p>Tại đây</p></button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="banner">
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+			  	<ol class="carousel-indicators">
+				    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				    <li data-target="#myCarousel" data-slide-to="1"></li>
+				    <li data-target="#myCarousel" data-slide-to="2"></li>
+				    <li data-target="#myCarousel" data-slide-to="3"></li>
+				    <li data-target="#myCarousel" data-slide-to="4"></li>
+			  	</ol>
 
-<script>
-document.getElementById("01").style.display = "none";
-function show()
-{
-	document.getElementById("01").style.display = "block";
-}
-function hide()
-{
-	document.getElementById("01").style.display = "none";
-}
-</script>
+			  	<div class="carousel-inner">
+				    	<div class="item active">
+				      		<a href="#"><img src="public/images/banner-1.png"/></a>
+				      		<div class="bg"></div>
+					      	<div class="banner-tt">
+								<h1>Ứng dụng miễn phí giúp bạn nhớ ngay <br> 10 từ mới mỗi ngày.</h1>
+								<div class="btn">
+									<button><p>Cài đặt miễn phí</p><p>Tại đây</p></button>
+								</div>
+					      	</div>
+				    	</div>
+				    	<div class="item">
+					      	<a href="#"><img src="public/images/banner-2.png"/></a>
+					      	<div class="bg"></div>
+					      	<div class="hero">
+					      	<p>"Tự động" nhớ 10 từ mỗi ngày</p>
+					      	<img src="public/images/p4.png" alt="">
+					      	</div>
+					    </div>
+					    <div class="item">
+					      	<a href="#"><img src="public/images/banner5.png"/></a>
+					      	<div class="bg"></div>
+					      	<div class="hero">
+					      	<p>Luyện tập ngay các từ vựng vừa học</p>
+					      	<img src="public/images/p1.png" alt="">
+					      	</div>
+					    </div>
+					    <div class="item">
+					      	<a href="#"><img src="public/images/banner-3.png"/></a>
+					      	<div class="bg"></div>
+					      	<div class="hero">
+					      	<p>Thách đấu với máy hoặc với học viên khác</p>
+					      	<img src="public/images/p2.png" alt="">
+					      	</div>
+					    </div>
+					    <div class="item">
+					      	<a href="#"><img src="public/images/banner-4.png"/></a>
+					      	<div class="bg"></div>
+					      	<div class="hero">
+					      	<p>Luyện phát âm chuẩn đến từng nanomet</p>
+					      	<img src="public/images/p3.png" alt="">
+					      	</div>
+					    </div>
+			  	</div>
+			</div>
+	</div>
+	<div class="noidung">
+		<div class="container">
+			<div class="title">
+				<p>ỨNG DỤNG SỐ 1 VỀ HỌC TỪ MỚI Ở VIỆT NAM</p>
+			</div>
+			<div class="row">
+				<div class="col-xs-6 col-sm-3 col-md-3 col-lg-3 nd">
+					<img src="public/images/search.png" alt="">
+					<p class="p2">Nhớ từ mới "Tự Động"</p>
+					<div class="border-bt"></div>
+					<p class="p1">Học với đầy đủ hình ảnh, phát âm, ví dụ. Không chỉ biết mặt từ mà còn hiểu cách áp dụng.</p>
+				</div>
+				<div class="col-xs-6 col-sm-3 col-md-3 col-lg-3 nd">
+					<img src="public/images/paper.png" alt="">
+					<p class="p2">Kiểm tra trực tiếp</p>
+					<div class="border-bt"></div>
+					<p class="p1">Với hình thức trắc nghiệm, Bạn sẽ kiểm tra được trí nhớ của mình sau mỗi bà</p>
+				</div>
+				<div class="col-xs-6 col-sm-3 col-md-3 col-lg-3 nd">
+					<img src="public/images/person.png" alt="">
+					<p class="p2">Luyện phát âm</p>
+					<div class="border-bt"></div>
+					<p class="p1">Lỗi phát âm sẽ được tô đỏ sau mỗi lần nói. Giúp bạn dễ dàng điều chỉnh và cải thiện khả năng phát âm</p>
+				</div>
+				<div class="col-xs-6 col-sm-3 col-md-3 col-lg-3 nd">
+					<img src="public/images/many.png" alt="">
+					<p class="p2">Thách đấu với bạn bè</p>
+					<div class="border-bt"></div>
+					<p class="p1">Với hình thức tương tác trực tiếp sinh động. Bạn sẽ nhớ bài lâu hơn và hình thành phản xạ tốt hơn.</p>
+				</div>												
+			</div>
+			<div class="btn">
+				<button><p>Cài đặt miễn phí</p><p>Tại đây</p></button>
+			</div>
+		</div>
+	</div>
+	<div class="congdong">
+		<div class="container">
+			<p>Cùng tham gia cộng đồng học tiếng Anh cùng<br>Elight với 50,000 người dùng khác tại đây.</p>
+			<img src="public/images/banner-5.png" alt="">
+			<div class="btn">
+				<button><p>Cài đặt miễn phí</p><p>Tại đây</p></button>
+			</div>
+		</div>
+	</div>
+	<div class="footer">
+		<div class="container">
+			<div class="row ft">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+					<img src="public/images/elight.png" alt="">
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 ftp">
+					<p>Địa chỉ: tầng 6, số 7, ngõ Chùa Nền,<br>Láng Thượng, Đống Đa, Hà Nội</p>
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 ftp">
+					<p>Mọi thông tin chi tiết xin liên hệ<br>elight.learningenglish@gmail.com</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script>
+		$('.carousel').carousel({
+			pause: "true"
+		});
+	</script>
+	<!-- <script src="js/owl.carousel.min.js"></script>
+	<script type="text/javascript">
+	$('.owl-carousel').owlCarousel({
+	pause:"false",
+    stagePadding: 0,
+    loop:true,
+    margin:0,
+    nav:false,
+    autoplay:true,
+	autoplayTimeout:3000,
+	autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+</script> -->
 </body>
 </html>
